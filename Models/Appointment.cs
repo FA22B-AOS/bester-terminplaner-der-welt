e6ea4369bbaae3389a2274d88ba66834d4571ca7
Hellsing <150229110+Spencernator@users.noreply.github.com>
2024-10-25 10:57:09 +0200

    Initial release

---
+++ Models/Appointment.cs
@@ -0,0 +1,10 @@
+﻿namespace WpfApp1.Models
+{
+    public class Appointment
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string Title { get; set; }
+        public DateTime Date { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ ViewModels/AppointmentViewModel.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.ObjectModel;
+using System.Windows.Input;
+using WpfApp1.Models;
+
+namespace WpfApp1.ViewModels
+{
+    public class AppointmentViewModel : ViewModelBase
+    {
+        public ObservableCollection<Appointment> Appointments { get; set; }
+        public Appointment NewAppointment { get; set; }
+
+        public ICommand AddAppointmentCommand { get; set; }
+        public ICommand DeleteAppointmentCommand { get; set; }
+
+        public AppointmentViewModel()
+        {
+            Appointments = new ObservableCollection<Appointment>();
+            NewAppointment = new Appointment();
+
+            AddAppointmentCommand = new RelayCommand(AddAppointment);
+            DeleteAppointmentCommand = new RelayCommand<Appointment>(DeleteAppointment);
+        }
+
+        private void AddAppointment()
+        {
+            if (!string.IsNullOrWhiteSpace(NewAppointment.Title) && NewAppointment.Date > DateTime.Now)
+            {
+                Appointments.Add(new Appointment
+                {
+                    Title = NewAppointment.Title,
+                    Date = NewAppointment.Date,
+                    Description = NewAppointment.Description
+                });
+                NewAppointment = new Appointment(); // Reset the form
+                OnPropertyChanged(nameof(NewAppointment));
+            }
+        }
+
+        private void DeleteAppointment(Appointment appointment)
+        {
+            if (appointment != null)
+            {
+                Appointments.Remove(appointment);
+            }
+        }
+    }
+}
